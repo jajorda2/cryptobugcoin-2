7ac548e30a4eaa4e9c8c03cd52c4c27e00da7ec9
jajorda2 <35240709+jajorda2@users.noreply.github.com>
2018-01-12 14:22:20 -0500

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {NULL, NULL}
+    {"cryptobugcoin.com", "165.227.222.8"}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
